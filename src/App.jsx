79dfe5f7e2625c434656cf2f559e5417871cde2e
13_Muhammad fauzan arief <fauzanaaa751@gmail.com>
2024-06-10 15:33:05 +0700

    tesst

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
 
 function App() {
   return (
+    <>
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
+      <ScrollControl pages={4} damping={0.1}>
       <Experience />
+      <Scroll html>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </Scroll>
+      </ScrollControl>
     </Canvas>
-
     
+    </>
   );
 }
 
